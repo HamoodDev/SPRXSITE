b351754672ff374deb2870de18b781bd66ea6eae
HamoodDev <30323480+HamoodDev@users.noreply.github.com>
2017-09-19 20:14:15 -0700

    Create Footer.php

---
+++ dist/Footer.php
@@ -0,0 +1,10 @@
+<?php
+
+?>
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Version:</b> <?php echo Version; ?> By <a href="http://HamoodDev.xyz" target="_blank"><b>HamoodDev</b></a>
+    </div>
+    <strong>Copyright &copy; <?php echo date("Y"); ?> <a href="<?php echo SiteURL; ?>"><?php echo Creator; ?></a>.</strong> All rights
+    reserved.
+</footer>
