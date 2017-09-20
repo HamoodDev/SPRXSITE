702fc1b213be10da3d42236a5dd594f94e465eef
HamoodDev <30323480+HamoodDev@users.noreply.github.com>
2017-09-19 20:11:39 -0700

    Create configs.php

---
+++ config/configs.php
@@ -0,0 +1,14 @@
+<?php
+    define("Version", "2.0", TRUE);
+    define("Creator", "HamoodDev", TRUE);
+    define("SiteName", "DevSite", TRUE);
+    define("SiteDescription", "", TRUE);
+    define("SiteType", "SPRX", TRUE);
+    define("SiteURL", "http://localhost/DevSite", TRUE);
+    define("SiteEmail", "admin@localhost.com", TRUE);
+	
+    define("HOST", "localhost");
+    define("USER", "root");
+    define("PASS", "");
+    define("DBNAME", "devsite");
+?>
